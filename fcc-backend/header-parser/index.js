8f2334c8711d03a59ef34adb8f9f4ae03e1f9818
anjapou <armand2ngassu@gmail.com>
2024-01-03 00:24:06 +0100

    header-parser

---
+++ fcc-backend/header-parser/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.get('/api/whoami', function (req, res) {
+    res.json({
+        'ip': req.headers['x-forwarded-for'],
+        'language': req.header('Accept-Language'),
+        'software': req.header('User-Agent'),
+    });
+});
+
+
+var listener = app.listen(process.env.PORT || 3000, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
